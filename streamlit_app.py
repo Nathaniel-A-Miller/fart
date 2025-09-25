2985c72aae6bceea3d972eaca8a982e386402daa
Nathaniel Ashton Miller <65291011+Nathaniel-A-Miller@users.noreply.github.com>
2025-09-25 07:33:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,17 @@ import streamlit as st
 
 st.title("💭 Writing Check")
 
-# Ask the question
-answer = st.radio("Are you thinking about how bad your writing is?", ["Yes", "No"])
+# Radio with no default selection on page load
+answer = st.radio(
+    "Are you thinking about how bad your writing is?",
+    ["Yes", "No"],
+    index=None
+)
 
 if answer == "No":
     st.success("Keep up the good work. 🎉")
-else:
+
+elif answer == "Yes":
     # Direct link with dl=1 to get the raw mp3
     url = "https://www.dropbox.com/scl/fi/3xhllqzptq2wadc163r9q/proud-fart-288263.mp3?rlkey=9eksxolgj1vrdqvmp2d9qrx6e&st=680xm7tv&dl=1"
     
